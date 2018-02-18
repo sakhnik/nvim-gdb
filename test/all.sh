4e4e2aea841e53e7d54034f58e289284e433caa5
Anatolii Sakhnik <sakhnik@gmail.com>
2018-02-18 08:00:41 +0200

    Improve cleanup

--- test/all.sh
+++ test/all.sh
@@ -8,17 +8,27 @@ echo -n "Check for gdb      " && which gdb
 echo -n "Check for lldb     " && which lldb
 echo -n "Check for python3  " && which python3
 
+cleanup_action="echo 'cleanup'"
+
+add_cleanup_action() {
+    cleanup_action="$cleanup_action; $@"
+}
+
+cleanup() {
+    eval "$cleanup_action"
+}
+
+trap cleanup EXIT
+
 if [[ $# -gt 0 ]]; then
     export NVIM_LISTEN_ADDRESS=/tmp/nvimtest
+    rm -rf $NVIM_LISTEN_ADDRESS
 
     # Run the test suite with a visible neovim
     nvim -n -u init.vim &
-    python3 -m unittest
+    add_cleanup_action "kill -KILL `jobs -p`; wait; reset"
 
-    # Cleanup the terminal
-    kill -KILL %1
-    wait
-    reset
+    python3 -m unittest
 else
     # Run the test suite with embedded neovim
     python3 -m unittest -v
