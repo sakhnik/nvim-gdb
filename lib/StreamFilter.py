27e34361c9fe9a45638c7a348493ea2161078fc3
Anatolii Sakhnik <sakhnik@gmail.com>
2018-07-18 09:57:15 +0300

    Extract inner class

--- lib/StreamFilter.py
+++ lib/StreamFilter.py
@@ -1,38 +1,38 @@
-#!/usr/bin/env python3
 """Filter the stream from within given pair of tokens."""
+
+
+class _StringMatcher:
+    def __init__(self, s, hold, succeed, fail):
+        self.s = s
+        self.hold = hold
+        self.succeed = succeed
+        self.fail = fail
+        self.idx = 0
+
+    def match(self, ch):
+        if self.s[self.idx] == ch:
+            self.idx += 1
+            if self.idx == len(self.s):
+                self.idx = 0
+                return self.succeed
+            return self.hold
+        self.idx = 0
+        return self.fail
 
 
 class StreamFilter:
     """Stream filter class."""
 
-    class _StringMatcher:
-        def __init__(self, s, hold, succeed, fail):
-            self.s = s
-            self.hold = hold
-            self.succeed = succeed
-            self.fail = fail
-            self.idx = 0
-
-        def match(self, ch):
-            if self.s[self.idx] == ch:
-                self.idx += 1
-                if self.idx == len(self.s):
-                    self.idx = 0
-                    return self.succeed
-                return self.hold
-            self.idx = 0
-            return self.fail
-
     def __init__(self, start, finish):
         """Initialize the filter with start and finish tokens."""
-        self.passing = StreamFilter._StringMatcher(start,
-                                                   self._StartHold,
-                                                   self._StartMatch,
-                                                   self._StartFail)
-        self.rejecting = StreamFilter._StringMatcher(finish,
-                                                     self._Nop,
-                                                     self._FinishMatch,
-                                                     self._Nop)
+        self.passing = _StringMatcher(start,
+                                      self._StartHold,
+                                      self._StartMatch,
+                                      self._StartFail)
+        self.rejecting = _StringMatcher(finish,
+                                        self._Nop,
+                                        self._FinishMatch,
+                                        self._Nop)
         self.state = self.passing
         self.buffer = ''
 
