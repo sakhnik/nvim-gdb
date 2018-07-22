50a27c7505798e1cd6a7ece39c97461c46c1f152
Anatolii Sakhnik <sakhnik@gmail.com>
2018-07-22 12:44:30 +0300

    Create gdb commands to query breakpoints of a given file

--- lib/gdb_commands.py
+++ lib/gdb_commands.py
@@ -26,3 +26,33 @@ class InfoSources(gdb.Command):
 
 
 InfoSources()
+
+
+class InfoBreakpoints(gdb.Command):
+    """
+    A custom GDB command to list all breakpoints for a given source file
+    """
+
+    def __init__(self):
+        """Register the command."""
+        super(InfoBreakpoints, self).__init__("nvim-gdb-info-breakpoints",
+                                              gdb.COMMAND_NONE)
+
+    def invoke(self, arg, from_tty):
+        """Execute the command."""
+        args = arg.split(" ")
+        path = args[0]        # Path to the listed file
+        sockaddr = args[1]    # Local socket address to send the result to
+
+        # List all breakpoints.
+        output = gdb.execute('info breakpoints', from_tty=False, to_string=True)
+        # Parse the output for (file, line)
+        locations = ((f.group(1), f.group(2)) for f in re.finditer(r' at ([^:]+):(\d+)', output))
+        # Filter breakpoints supposedly belonging to the file in question
+        breaks = [ln for (fl, ln) in locations if path.endswith(fl)]
+        # Send the result to the given local socket
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        sock.sendto(" ".join(breaks).encode('utf-8'), sockaddr)
+
+
+InfoBreakpoints()
