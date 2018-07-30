0d7ac96eef69adddc2e28a14379635053357fbdc
Anatolii Sakhnik <sakhnik@gmail.com>
2018-07-30 22:04:06 +0300

    Query actual breakpoints from lldb

--- lib/lldb_commands.py
+++ lib/lldb_commands.py
@@ -2,6 +2,36 @@ import threading
 import os
 import socket
 import re
+import json
+import lldb
+
+
+# Get list of enabled breakpoints for a given source file
+def _GetBreaks(fname):
+    breaks = {}
+
+    # Ensure target is the actually selected one
+    target = lldb.debugger.GetSelectedTarget()
+
+    # Consider every breakpoint while skipping over the disabled ones
+    for breakpoint in target.breakpoint_iter():
+        if not breakpoint.IsEnabled():
+            continue
+        bid = breakpoint.GetID()
+
+        # Consider every location of a breakpoint
+        for loc in breakpoint:
+            lineentry = loc.GetAddress().GetLineEntry()
+            filespec = lineentry.GetFileSpec()
+            path = os.path.join(filespec.GetDirectory(),
+                                filespec.GetFilename())
+
+            # See whether the breakpoint is in the file in question
+            if fname == path:
+                breaks[lineentry.GetLine()] = bid
+
+    # Return the filtered breakpoints
+    return json.dumps(breaks)
 
 
 def server(server_address):
@@ -19,7 +49,8 @@ def server(server_address):
         command = re.split(r'\s+', data.decode('utf-8'))
         if command[0] == "info-breakpoints":
             fname = command[1]
-            sock.sendto(fname.encode('utf-8'), 0, addr)
+            breaks = _GetBreaks(fname)
+            sock.sendto(breaks.encode('utf-8'), 0, addr)
 
 
 def init(debugger, command, result, internal_dict):
