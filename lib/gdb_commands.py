0daf999efeeeaf5805917a2966ceefc65dda3058
Anatolii Sakhnik <sakhnik@gmail.com>
2018-07-31 20:43:05 +0300

    Remove unused command

--- lib/gdb_commands.py
+++ lib/gdb_commands.py
@@ -4,31 +4,6 @@ import socket
 import json
 
 
-class InfoSources(gdb.Command):
-    """
-    A custom GDB command to list all source files and send it to a socket.
-
-    The files names will be reversed and sorted to simplify searching
-    by basename.
-    """
-
-    def __init__(self):
-        """Register the command."""
-        super(InfoSources, self).__init__("nvim-gdb-info-sources",
-                                          gdb.COMMAND_NONE)
-
-    def invoke(self, arg, from_tty):
-        """Execute the command."""
-        output = gdb.execute('info sources', from_tty=False, to_string=True)
-        sources = "\n".join(sorted([f.group(0)[::-1] for f in
-                                    re.finditer(r'/[^, \n]+', output)]))
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-        sock.sendto(sources.encode('utf-8'), arg)
-
-
-InfoSources()
-
-
 class InfoBreakpoints(gdb.Command):
     """
     A custom GDB command to list all breakpoints for a given source file
