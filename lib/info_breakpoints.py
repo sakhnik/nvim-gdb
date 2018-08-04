72bdbe583889421e9957c8bce6b46f1c4cecaf1a
Anatolii Sakhnik <sakhnik@gmail.com>
2018-08-04 08:14:05 +0300

    Fix #26: assign random socket name to query proxies

--- lib/info_breakpoints.py
+++ lib/info_breakpoints.py
@@ -3,39 +3,29 @@ import vim
 import socket
 import sys
 import os
+import tempfile
 
 
-# TODO: generate random rendez-vous point
-server_address = '/tmp/nvim-gdb-python-socket'
+# Let's put our unix socket into a random directory to avoid races
+with tempfile.TemporaryDirectory() as dname:
 
-# Make sure the socket does not already exist
-try:
-    os.unlink(server_address)
-except OSError:
-    if os.path.exists(server_address):
-        raise
+    # Create a UDS socket
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    # Bind the socket to be able to receive responses
+    sock.bind(os.path.join(dname, 'sock'))
+    # Break out after 1/2 second
+    sock.settimeout(0.5)
 
-# Create a UDS socket
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-sock.bind(server_address)
-sock.settimeout(0.5)
+    # File in question
+    fname = sys.argv[0]
+    proxy_addr = sys.argv[1]
 
-# File in question
-fname = sys.argv[0]
-proxy_addr = sys.argv[1]
+    # Let GDB send back the list of sources
+    command = 'info-breakpoints %s\n' % fname
+    sock.sendto(command.encode('utf-8'), 0, proxy_addr)
 
-# Let GDB send back the list of sources
-command = 'info-breakpoints %s\n' % fname
-sock.sendto(command.encode('utf-8'), 0, proxy_addr)
+    # Receive the result from GDB
+    data, addr = sock.recvfrom(65536)
 
-# Receive the result from GDB
-data, addr = sock.recvfrom(65536)
-
-# Get rid of the rendez-vous point
-try:
-    os.unlink(server_address)
-except OSError:
-    pass
-
-# Set return value for Vim
-vim.command("let return_value = '" + data.decode('utf-8') + "'")
+    # Set return value for Vim
+    vim.command("let return_value = '" + data.decode('utf-8') + "'")
