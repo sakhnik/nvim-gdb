92ecb08534f717cd05138df46176a0dc9a1fad9c
Anatolii Sakhnik <sakhnik@gmail.com>
2018-08-06 20:44:24 +0300

    Adapt prerequisites.sh for Darwin

--- test/prerequisites.sh
+++ test/prerequisites.sh
@@ -2,13 +2,17 @@
 
 # Check the prerequisites
 echo -n "Check for neovim     " && which nvim
-echo -n "Check for gdb        " && which gdb
+[[ `uname` != Darwin ]] && \
+    echo -n "Check for gdb        " && which gdb
 echo -n "Check for lldb       " && which lldb
 echo -n "Check for python3    " && which python3
 
+CXX=g++
+[[ `uname` == Darwin ]] && CXX=clang++
+
 echo -n "Compiling test.cpp   "
 if [[ src/test.cpp -nt a.out || src/lib.hpp -nt a.out ]]; then
-    g++ -g -std=c++11 src/test.cpp
+    $CXX -g -std=c++11 src/test.cpp
     echo "a.out"
 else
     echo "(cached a.out)"
