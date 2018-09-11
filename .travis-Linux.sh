de7d1f2526f8a88ad4f82183cbb2a2e4726a73a3
Anatolii Sakhnik <sakhnik@gmail.com>
2018-09-11 21:33:34 +0300

    Use the apt addon in Linux

--- .travis-Linux.sh
+++ .travis-Linux.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -x
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-5.0 main" | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get -qq update
-sudo apt-get install -y g++ gdb lldb-5.0
-sudo apt-get install python3-dev python3-pip
-sudo ln -sf /usr/bin/lldb-5.0 /usr/bin/lldb
-
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod +x nvim.appimage
 ./nvim.appimage --appimage-extract
@@ -18,3 +10,5 @@ cat >$HOME/bin/nvim <<EOF
 `pwd`/squashfs-root/usr/bin/nvim "\$@"
 EOF
 chmod +x $HOME/bin/nvim
+
+ln -sf /usr/bin/lldb-5.0 $HOME/bin/lldb
