8feb78994552777b2384393a2ee69050f10f525e
Anatolii Sakhnik <sakhnik@gmail.com>
2018-09-11 22:27:49 +0300

    Use stable version of neovim

--- .travis-Darwin.sh
+++ .travis-Darwin.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/bin
 
 pip install --user six
 
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/v0.3.1/nvim-macos.tar.gz
 tar -xf nvim-macos.tar.gz
 cat >$HOME/bin/nvim <<EOF
 #!/bin/bash
