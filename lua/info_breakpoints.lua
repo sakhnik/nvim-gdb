057b7919a316d34f4c71f4621ca94f94bd37edca
Anatolii Sakhnik <sakhnik@gmail.com>
2018-11-03 11:54:35 +0200

    Rename libsock -> s

--- lua/info_breakpoints.lua
+++ lua/info_breakpoints.lua
@@ -1,14 +1,14 @@
 libstd = require "posix.stdlib"
-libsock = require "posix.sys.socket"
+s = require "posix.sys.socket"
 
 function InfoBreakpoints(fname, proxy_addr)
     dir = libstd.mkdtemp('/tmp/nvimgdb-sock-XXXXXX')
-    sock = libsock.socket(libsock.AF_UNIX, libsock.SOCK_DGRAM, 0)
-    libsock.bind(sock, {family = libsock.AF_UNIX, path = dir .. "/socket"})
-    libsock.setsockopt(sock, libsock.SOL_SOCKET, libsock.SO_RCVTIMEO, 0, 500000)
-    libsock.connect(sock, {family = libsock.AF_UNIX, path = proxy_addr})
-    libsock.send(sock, "info-breakpoints " .. fname .. "\n")
-    data = libsock.recv(sock, 65536)
+    sock = s.socket(s.AF_UNIX, s.SOCK_DGRAM, 0)
+    s.bind(sock, {family = s.AF_UNIX, path = dir .. "/socket"})
+    s.setsockopt(sock, s.SOL_SOCKET, s.SO_RCVTIMEO, 0, 500000)
+    s.connect(sock, {family = s.AF_UNIX, path = proxy_addr})
+    s.send(sock, "info-breakpoints " .. fname .. "\n")
+    data = s.recv(sock, 65536)
     os.remove(dir .. "/socket")
     os.remove(dir)
     return data
