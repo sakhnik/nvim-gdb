42c392c2f5985a5ecd05f241421f09a97c791842
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-23 18:40:12 +0200

    No need to install rexlib

--- install.sh
+++ install.sh
@@ -36,7 +36,6 @@ EOF
 $luarocks install luaposix --tree="$rocks_tree"
 $luarocks install moonscript --tree="$rocks_tree"
 $luarocks install json-lua --tree="$rocks_tree"
-$luarocks install Lrexlib-PCRE --tree="$rocks_tree"
 
 # Compile all moon scripts
 find lua -name '*.moon' -exec "$rocks_tree/bin/moonc" {} \;
