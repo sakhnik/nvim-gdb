69e0bac5ae38b226e6c35e7b004f03d24724af54
Anatolii Sakhnik <sakhnik@gmail.com>
2019-02-15 21:26:40 +0200

    Start diagnosing test_conflict

--- test/all.sh
+++ test/all.sh
@@ -7,7 +7,7 @@ cd `dirname ${BASH_SOURCE[0]}`
 python3 ../lib/StreamFilter.py
 
 if [[ $# -gt 0 ]]; then
-    ./run-visual -vv
+    ./run-visual -vv test_40_keymap.py::test_conflict
 else
     ./run -vv
 fi
