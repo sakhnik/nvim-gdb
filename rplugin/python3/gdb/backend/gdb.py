f72f4c67e21b7aff12d4671fc1cbc835fa28cb85
Anatolii Sakhnik <sakhnik@gmail.com>
2019-02-16 10:29:16 +0200

    Fix the race in the interrupt test

--- rplugin/python3/gdb/backend/gdb.py
+++ rplugin/python3/gdb/backend/gdb.py
@@ -8,12 +8,13 @@ class GdbScm(BaseScm):
     def __init__(self, vim, cursor, win):
         super().__init__(vim, cursor, win)
 
-        self.addTrans(self.paused,  re.compile(r'^Continuing\.'),              self.pausedContinue)
-        self.addTrans(self.paused,  re.compile(r'^\x1a\x1a([^:]+):(\d+):\d+'), self.pausedJump)
-        self.addTrans(self.paused,  re.compile(r'^\(gdb\) $'),                 self.queryB)
-        self.addTrans(self.running, re.compile(r'^Breakpoint \d+'),            self.queryB)
-        self.addTrans(self.running, re.compile(r' hit Breakpoint \d+'),        self.queryB)
-        self.addTrans(self.running, re.compile(r'^\(gdb\) $'),                 self.queryB)
+        re_jump = re.compile(r'^\x1a\x1a([^:]+):(\d+):\d+')
+        self.addTrans(self.paused,  re.compile(r'^Continuing\.'),       self.pausedContinue)
+        self.addTrans(self.paused,  re_jump,                            self.pausedJump)
+        self.addTrans(self.paused,  re.compile(r'^\(gdb\) $'),          self.queryB)
+        self.addTrans(self.running, re.compile(r'^Breakpoint \d+'),     self.queryB)
+        self.addTrans(self.running, re.compile(r'^\(gdb\) $'),          self.queryB)
+        self.addTrans(self.running, re_jump,                            self.pausedJump)
 
         self.state = self.running
 
