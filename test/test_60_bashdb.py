f0f934933e3dd8134616ca366dd1724357f27145
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-11 07:11:16 +0300

    Test breakpoints in bashdb

--- test/test_60_bashdb.py
+++ test/test_60_bashdb.py
@@ -38,3 +38,20 @@ def test_smoke(eng, post):
 
     eng.feed('<f5>')
     assert eng.waitSigns({}, 1500) is None
+
+def test_break(eng, post):
+    # Test toggling breakpoints.
+    eng.feed(' db')
+    eng.feed('\n', 1500)
+    eng.feed('<esc>')
+
+    eng.feed('<esc><c-w>k')
+    eng.feed(':4<cr>')
+    eng.feed('<f8>')
+    assert {'cur': 'main.sh:22', 'break': {1: [4]}} == eng.getSigns()
+
+    eng.exe('GdbContinue', 300)
+    assert {'cur': 'main.sh:4', 'break': {1: [4]}} == eng.getSigns()
+
+    eng.feed('<f8>')
+    assert eng.waitSigns({'cur': 'main.sh:4'}) is None
