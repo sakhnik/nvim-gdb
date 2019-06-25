94ea225b3c68c707cc1c7d823fe4c4dda49b65ec
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-25 15:35:35 +0300

    Fix parameters shadowing built-in

--- lib/StreamFilter.py
+++ lib/StreamFilter.py
@@ -3,9 +3,9 @@
 
 class Filter:
     """Pass-through filter."""
-    def filter(self, input):
-        """Process input, filter between tokens, return the output."""
-        return input, None
+    def filter(self, data):
+        """Process data, filter between tokens, return the output."""
+        return data, None
 
     def timeout(self):
         """Process timeout, return whatever was kept in the buffer."""
@@ -24,15 +24,15 @@ class StreamFilter(Filter):
     def updateFinishMatcher(self, finish_re):
         self.matcher = finish_re
 
-    # Accept the input: either append it to the buffer until
+    # Accept the data: either append it to the buffer until
     # the final matcher has been met, or output the whole filtered buffer.
     # Returns a tuple (bytes to show, bytes suppressed until and including the finish matcher).
-    def filter(self, input, buffer_callback=lambda _: None):
-        """Process input, filter until the finish match, return the output."""
+    def filter(self, data):
+        """Process data, filter until the finish match, return the output."""
         filtered = None
         if not self.matcher:
-            return input, None
-        self.buffer.extend(input)
+            return data, None
+        self.buffer.extend(data)
         # XXX: note that we are scanning over the buffer again and again
         # if this causes noticeable performance issue, consider maintaining
         # a smaller part of the buffer to scan.
