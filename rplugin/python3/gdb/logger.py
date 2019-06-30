7bfcbca298970ef22a0644a7f90010526b67e6d4
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-30 19:54:14 +0300

    Lint proxy.py

--- rplugin/python3/gdb/logger.py
+++ rplugin/python3/gdb/logger.py
@@ -1,13 +1,21 @@
+'''.'''
+
 import os
 
+
 class Logger:
+    '''The logger configurable with environment variables.'''
     def __init__(self):
         fname = os.getenv('NVIMGDB_LOGFILE')
-        self.f = None if not fname else open(fname, 'w')
+        self.fout = None if not fname else open(fname, 'w')
         keys = os.getenv('NVIMGDB_LOGKEYS')
         self.keys = {} if not keys else set(keys.split(','))
 
     def log(self, key, msg):
-        if self.f and key in self.keys:
-            self.f.write('[{}] {}\n'.format(key, msg))
-            self.f.flush()
+        '''Log a message identified by the key.'''
+        if self.fout and key in self.keys:
+            self.fout.write(f'[{key}] {msg}\n')
+            self.fout.flush()
+
+    def dummy(self):
+        '''Do nothing.'''
--- rplugin/python3/gdb/proxy.py
+++ rplugin/python3/gdb/proxy.py
@@ -1,42 +1,48 @@
+'''Connection to the side channel.'''
+
 import os
 import socket
 
 
 class Proxy:
-    def __init__(self, vim, proxyAddr, sockDir):
+    '''Proxy to the side channel.'''
+    def __init__(self, vim, proxy_addr, sock_dir):
         self.vim = vim
-        self.proxyAddr = proxyAddr
-        self.sockAddr = os.path.join(sockDir.get(), "client")
+        self.proxy_addr = proxy_addr
+        self.sock_addr = os.path.join(sock_dir.get(), "client")
 
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-        self.sock.bind(self.sockAddr)
+        self.sock.bind(self.sock_addr)
         self.sock.settimeout(0.5)
         # Will connect to the socket later, when the first query is needed
         # to be issued.
         self.connected = False
 
     def cleanup(self):
+        '''The destructor.'''
         if self.sock:
             self.sock.close()
         try:
-            os.remove(self.sockAddr)
+            os.remove(self.sock_addr)
         except FileNotFoundError:
             pass
 
-    def ensureConnected(self):
+    def _ensure_connected(self):
         if not self.connected:
             try:
-                self.sock.connect(self.proxyAddr)
+                self.sock.connect(self.proxy_addr)
                 self.connected = True
             except OSError as msg:
-                self.vim.command("echo 'Breakpoint: not connected to the proxy: %s'" % msg)
+                self.vim.command("echo 'Breakpoint: not connected"
+                                 f" to the proxy: {msg}'")
         return self.connected
 
     def query(self, request):
+        '''Send a request to the proxy and wait for the response.'''
         # It takes time for the proxy to open a side channel.
         # So we're connecting to the socket lazily during
         # the first query.
-        if self.ensureConnected():
+        if self._ensure_connected():
             self.sock.send(request.encode('utf-8'))
             return self.sock.recv(65536).decode('utf-8')
         return ''
