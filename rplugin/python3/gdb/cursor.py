2d12f1fdddbf724ab3a364d25c5205820fb7bd25
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-30 19:32:26 +0300

    Lint cursor.py

--- rplugin/python3/gdb/cursor.py
+++ rplugin/python3/gdb/cursor.py
@@ -1,4 +1,8 @@
+'''Manipulating the current line sign.'''
+
+
 class Cursor:
+    '''The current line sign operations.'''
     def __init__(self, vim):
         self.vim = vim
         self.buf = -1
@@ -6,27 +10,30 @@ class Cursor:
         self.sign_id = -1
 
     def hide(self):
+        '''Hide the current line sign.'''
         if self.sign_id != -1:
-            self.vim.command('sign unplace %d' % self.sign_id)
+            self.vim.command(f'sign unplace {self.sign_id}')
             self.sign_id = -1
 
     def show(self):
-        # To avoid flicker when removing/adding the sign column(due to the change in
-        # line width), we switch ids for the line sign and only remove the old line
-        # sign after marking the new one
+        '''Show the current line sign.'''
+        # To avoid flicker when removing/adding the sign column(due to
+        # the change in line width), we switch ids for the line sign
+        # and only remove the old line sign after marking the new one.
         old_sign_id = self.sign_id
         self.sign_id = 4999 + (4998 - old_sign_id if old_sign_id != -1 else 0)
         if self.line != -1 and self.buf != -1:
-            self.vim.command('sign place %d name=GdbCurrentLine line=%d buffer=%d'
-                % (self.sign_id, self.line, self.buf))
+            self.vim.command(f'sign place {self.sign_id} name=GdbCurrentLine'
+                             f' line={self.line} buffer={self.buf}')
         if old_sign_id != -1:
-            self.vim.command('sign unplace %d' % old_sign_id)
+            self.vim.command(f'sign unplace {old_sign_id}')
 
     def set(self, buf, line):
+        '''Set the current line sign number.'''
         self.buf = buf
         self.line = int(line)
 
-    # Redraw the cursor sign if it was visible before.
     def reshow(self):
+        '''Redraw the cursor sign if it was visible before.'''
         if self.sign_id != -1:
             self.show()
