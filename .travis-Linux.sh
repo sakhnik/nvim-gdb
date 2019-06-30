763db85a275b341867c16d589131597466eb7246
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-30 18:09:10 +0300

    Make sure python 3.7 is selected in Travis Linux

--- .travis-Linux.sh
+++ .travis-Linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+
+ln -sf /usr/bin/python3.7 "$HOME/bin/python3"
+ln -sf /usr/bin/python3.7-config "$HOME/bin/python3-config"
+
 # Install pip for python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
