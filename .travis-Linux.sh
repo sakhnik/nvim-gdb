13a8d2072b5e031031a61e693d3f170fcd093b8d
Anatolii Sakhnik <sakhnik@gmail.com>
2019-06-30 10:01:02 +0300

    Fix travis Linux build

--- .travis-Linux.sh
+++ .travis-Linux.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -x
 
+# Install pip for python3
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+
 curl -LO https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage
 chmod +x nvim.appimage
 ./nvim.appimage --appimage-extract
 
-mkdir -p $HOME/bin
-cat >$HOME/bin/nvim <<EOF
+mkdir -p "$HOME/bin"
+cat >"$HOME/bin/nvim" <<EOF
 #!/bin/bash
 $(pwd)/squashfs-root/usr/bin/nvim "\$@"
 EOF
-chmod +x $HOME/bin/nvim
+chmod +x "$HOME/bin/nvim"
 
-ln -sf /usr/bin/lldb-5.0 $HOME/bin/lldb
+ln -sf /usr/bin/lldb-7.0 "$HOME/bin/lldb"
