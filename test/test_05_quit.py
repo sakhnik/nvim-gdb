ea72e6f810a5670bd2cdebf234c619d621a02580
Anatolii Sakhnik <sakhnik@gmail.com>
2019-07-11 22:23:07 +0300

    Fix warnings in test_05_quit

--- test/test_05_quit.py
+++ test/test_05_quit.py
@@ -1,3 +1,7 @@
+'''Test quitting the debugging session.'''
+
+# pylint: disable=redefined-outer-name
+
 import pytest
 
 
@@ -7,7 +11,7 @@ def setup(eng):
     num_bufs = eng.count_buffers()
     eng.feed(":GdbStart ./dummy-gdb.sh<cr>")
     eng.feed('<esc>')
-    yield
+    yield True
     # Check that no new buffers have left
     assert num_bufs == eng.count_buffers()
     assert eng.eval("tabpagenr('$')") == 1
@@ -15,15 +19,18 @@ def setup(eng):
 
 def test_gdb_debug_stop(setup, eng):
     '''Quit with the command GdbDebugStop.'''
+    assert setup
     eng.feed(":GdbDebugStop<cr>")
 
 
-def test_terminal_ZZ(setup, eng):
+def test_terminal_zz(setup, eng):
     '''Quit with ZZ.'''
+    assert setup
     eng.feed("ZZ")
 
 
-def test_jump_ZZ(setup, eng):
+def test_jump_zz(setup, eng):
     '''Quit with ZZ from the jump window.'''
+    assert setup
     eng.feed("<c-w>w")
     eng.feed("ZZ")
