c682f4db1d1fd115ac1c5aa7c4a1b6a4f30d6e69
Anatolii Sakhnik <sakhnik@gmail.com>
2019-09-29 23:06:55 +0300

    Fix indentation

--- rplugin/python3/gdb/cursor.py
+++ rplugin/python3/gdb/cursor.py
@@ -15,7 +15,7 @@ class Cursor(Common):
         '''Hide the current line sign.'''
         if self.sign_id != -1:
             self.vim.call('sign_unplace', 'NvimGdb',
-                    {'id': self.sign_id, 'buffer': self.buf})
+                          {'id': self.sign_id, 'buffer': self.buf})
             self.sign_id = -1
 
     def show(self):
@@ -27,11 +27,11 @@ class Cursor(Common):
         self.sign_id = 4999 + (4998 - old_sign_id if old_sign_id != -1 else 0)
         if self.line != -1 and self.buf != -1:
             self.vim.call('sign_place', self.sign_id, 'NvimGdb',
-                    'GdbCurrentLine', self.buf,
-                    {'lnum': self.line, 'priority': 20})
+                          'GdbCurrentLine', self.buf,
+                          {'lnum': self.line, 'priority': 20})
         if old_sign_id != -1:
             self.vim.call('sign_unplace', 'NvimGdb',
-                    {'id': old_sign_id, 'buffer': self.buf})
+                          {'id': old_sign_id, 'buffer': self.buf})
 
     def set(self, buf, line):
         '''Set the current line sign number.'''
