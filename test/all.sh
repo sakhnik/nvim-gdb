250ed7c8409f8671389ebf48160199f1fb935d68
Anatolii Sakhnik <sakhnik@gmail.com>
2020-01-21 10:21:02 +0200

    Test #101 run the test suite from the symbolic run

--- test/all.sh
+++ test/all.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+rootDir="$(pwd -P)"
+
+# Deliberately test that the tests pass from a random symlink
+# to the source directory.
+tmpDir="$(mktemp -d /tmp/nvim-gdb-test-XXXXXX)"
+ln -sf "$rootDir" "$tmpDir/src"
+cleanup() {
+    unlink "$tmpDir/src";
+    rmdir "$tmpDir"
+}
+trap cleanup EXIT
+
+cd "$tmpDir/src/test"
 
 ./prerequisites.sh
 
