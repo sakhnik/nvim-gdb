d82a978d9cb69282ebf9efde177eecb2e45bc83f
Anatolii Sakhnik <sakhnik@gmail.com>
2021-01-03 13:41:56 +0200

    Try using homebrew to install bashdb

--- .ghwf-Linux.sh
+++ .ghwf-Linux.sh
@@ -9,3 +9,6 @@ ln -sf "$PWD/nvim.appimage" "$HOME/bin/nvim"
 
 sudo apt-get update
 sudo apt-get install gdb lldb
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+homebrew install bashdb
