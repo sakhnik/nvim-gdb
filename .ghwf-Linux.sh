c580f24e86a6dd8e3346ec9d7a6d4214d6eb68b6
Anatolii Sakhnik <sakhnik@gmail.com>
2021-01-03 08:35:36 +0200

    Deploy nvim to ~/bin

--- .ghwf-Linux.sh
+++ .ghwf-Linux.sh
@@ -4,6 +4,6 @@ source .travis-common
 
 curl -LO "$NVIM_RELEASE_URL/nvim.appimage"
 chmod +x nvim.appimage
-ln -sf "$PWD/nvim.appimage" /usr/local/bin/nvim
-
+mkdir -p "$HOME/bin"
+ln -sf "$PWD/nvim.appimage" "$HOME/bin/nvim"
 ln -sf /usr/bin/lldb-7.0 "$HOME/bin/lldb"
