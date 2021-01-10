7c5e9b0211e235a1edb3e7c7e55dcb118448d4c0
Anatolii Sakhnik <sakhnik@gmail.com>
2021-01-10 22:21:44 +0200

    Test pdb and bashdb

--- test/test_70_quickfix.py
+++ test/test_70_quickfix.py
@@ -3,8 +3,8 @@
 import time
 
 
-def test_backend(eng, backend):
-    '''Quickfix in C++.'''
+def test_breaks_backend(eng, backend):
+    '''Breakpoint quickfix in C++.'''
     eng.feed(backend['launch'])
     assert eng.wait_paused() is None
     eng.feed('b main\n')
@@ -22,3 +22,47 @@ def test_backend(eng, backend):
     assert eng.eval("line('.')") == 5
     eng.feed(':cnext\n')
     assert eng.eval("line('.')") == 5
+
+
+def test_breaks_pdb(eng, post):
+    '''Breakpoint quickfix in PDB.'''
+    assert post
+    eng.feed(' dp\n')
+    assert eng.wait_paused() is None
+    eng.feed('b _main\n')
+    eng.feed('b _foo\n')
+    eng.feed('b _bar\n')
+    eng.feed('<esc>')
+    eng.feed(':GdbCopenBreakpoints\n')
+    time.sleep(0.3)
+    eng.feed(':cc\n')
+    assert eng.wait_for(lambda: eng.eval("line('.')"), \
+            lambda r: r == 14) is None
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 8
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 4
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 4
+
+
+def test_breaks_bashdb(eng, post):
+    '''Breakpoint quickfix in BashDB.'''
+    assert post
+    eng.feed(' db\n')
+    assert eng.wait_paused() is None
+    eng.feed('b Main\n')
+    eng.feed('b Foo\n')
+    eng.feed('b Bar\n')
+    eng.feed('<esc>')
+    eng.feed(':GdbCopenBreakpoints\n')
+    time.sleep(0.3)
+    eng.feed(':cc\n')
+    assert eng.wait_for(lambda: eng.eval("line('.')"), \
+            lambda r: r == 16) is None
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 7
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 3
+    eng.feed(':cnext\n')
+    assert eng.eval("line('.')") == 3
