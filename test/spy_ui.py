798286cfaa238bfefd159e5f57c4aa6fbd7ba653
Anatolii Sakhnik <sakhnik@gmail.com>
2021-01-14 22:01:41 +0200

    Lint spy_ui.log

--- test/spy_ui.py
+++ test/spy_ui.py
@@ -1,30 +1,43 @@
+"""A UI client for neovim used to fetch screen logs for testing."""
+
 from pynvim import attach
 
 
 class SpyUI:
+    """Spy UI is a UI client for neovim.
+
+    It will attach to a running instance of neovim and capture its screen.
+    """
+
     def __init__(self):
+        """Constructor."""
         self.nvim = attach('tcp', address='localhost', port=44444)
         self.width = 80
         self.height = 25
-        self.cursor = (0, 0)
+        # Cursor row and col
+        self.row = 0
+        self.col = 0
+        self.scroll_region = (0, 0, 0, 0)
         self.nvim.ui_attach(self.width, self.height, True)
         self.grid = []
-        for r in range(self.height):
+        for _ in range(self.height):
             self.grid.append([' '] * self.width)
         self.screen = self.to_str()
 
     def run(self):
-        self.nvim.run_loop(self._req, self._not)
+        """Run the loop."""
+        def _req(name, arg):
+            print("req", name, arg)
+        self.nvim.run_loop(_req, self._not)
 
     def close(self):
+        """Break from the UI loop."""
         self.nvim.stop_loop()
 
-    def _req(self, name, arg):
-        print("req", name)
-
-    IGNORE_REDRAW = frozenset( \
+    IGNORE_REDRAW = frozenset(
             ("mode_info_set", "mode_change", "mouse_on", "mouse_off",
-             "option_set", "default_colors_set", "hl_group_set", "hl_attr_define",
+             "option_set", "default_colors_set",
+             "hl_group_set", "hl_attr_define",
              "grid_cursor_goto", "busy_start", "busy_stop",
              "update_fg", "update_bg", "update_sp", "highlight_set",
              "win_viewport"
@@ -33,64 +46,65 @@ class SpyUI:
 
     def _not(self, name, args):
         if name == "redraw":
-            for arg in args:
-                cmd = arg[0]
-                par = arg[1]
-                if cmd in self.IGNORE_REDRAW:
-                    continue
-                if cmd == "resize":
-                    self._resize(*par)
-                elif cmd == "clear":
-                    self._clear()
-                elif cmd == "eol_clear":
-                    self._eol_clear()
-                elif cmd == "cursor_goto":
-                    self._cursor_goto(*par)
-                elif cmd == "put":
-                    self._put(arg[1:])
-                elif cmd == "set_scroll_region":
-                    self._set_scroll_region(*par)
-                elif cmd == "scroll":
-                    self._scroll(*par)
-                elif cmd == "flush":
-                    self.screen = self.to_str()
-                    # self._print()
-                    pass
-                else:
-                    print(cmd, par)
+            self._redraw(args)
         else:
             print(name, args)
 
+    def _redraw(self, args):
+        for arg in args:
+            cmd = arg[0]
+            par = arg[1]
+            if cmd in self.IGNORE_REDRAW:
+                continue
+            if cmd == "resize":
+                self._resize(*par)
+            elif cmd == "clear":
+                self._clear()
+            elif cmd == "eol_clear":
+                self._eol_clear()
+            elif cmd == "cursor_goto":
+                self._cursor_goto(*par)
+            elif cmd == "put":
+                self._put(arg[1:])
+            elif cmd == "set_scroll_region":
+                self._set_scroll_region(*par)
+            elif cmd == "scroll":
+                self._scroll(*par)
+            elif cmd == "flush":
+                self.screen = self.to_str()
+                # self._print()
+            else:
+                print(cmd, par)
+
     def _resize(self, width, height):
         new_grid = []
-        for r in range(height):
+        for _ in range(height):
             new_grid.append([' '] * width)
-        for r in range(min(self.height, height)):
-            for c in range(min(self.width, width)):
-                new_grid[r][c] = self.grid[r][c]
+        for row in range(min(self.height, height)):
+            for col in range(min(self.width, width)):
+                new_grid[row][col] = self.grid[row][col]
         self.grid = new_grid
         self.width = width
         self.height = height
 
     def _clear(self):
-        for r in range(self.height):
-            for c in range(self.width):
-                self.grid[r][c] = ' '
+        for row in range(self.height):
+            for col in range(self.width):
+                self.grid[row][col] = ' '
 
     def _eol_clear(self):
-        row, col = self.cursor
-        for c in range(col, self.width):
-            self.grid[row][c] = ' '
+        for col in range(self.col, self.width):
+            self.grid[self.row][col] = ' '
 
     def _cursor_goto(self, row, col):
-        self.cursor = (row, col)
+        self.row = row
+        self.col = col
 
     def _put(self, text):
-        row, col = self.cursor
-        r = self.grid[row]
-        for i in range(len(text)):
-            r[col + i] = text[i][0]
-        self.cursor = (row, col + len(text))
+        text_row = self.grid[self.row]
+        for col, char in enumerate(text):
+            text_row[self.col + col] = char[0]
+        self.col += len(text)
 
     def _set_scroll_region(self, top, bot, left, right):
         self.scroll_region = (top, bot, left, right)
@@ -98,21 +112,22 @@ class SpyUI:
     def _scroll(self, rows):
         top, bot, left, right = self.scroll_region
         if rows > 0:
-            for r in range(top, bot - rows):
-                rfrom = r + rows
-                for c in range(left, right):
-                    self.grid[r][c] = self.grid[rfrom][c]
+            for row in range(top, bot - rows):
+                rfrom = row + rows
+                for col in range(left, right):
+                    self.grid[row][col] = self.grid[rfrom][col]
         else:
-            for r in range(bot - 1, top - rows - 1, -1):
-                rfrom = r + rows
-                for c in range(left, right):
-                    self.grid[r][c] = self.grid[rfrom][c]
+            for row in range(bot - 1, top - rows - 1, -1):
+                rfrom = row + rows
+                for col in range(left, right):
+                    self.grid[row][col] = self.grid[rfrom][col]
 
     def to_str(self):
+        """Render the grid into a string."""
         lines = []
         lines.append("+" + "-" * self.width + "+")
-        for r in range(len(self.grid)):
-            lines.append('|' + ''.join(self.grid[r]) + '|')
+        for row in self.grid:
+            lines.append('|' + ''.join(row) + '|')
         lines.append("+" + "-" * self.width + "+")
         return "\n".join(lines)
 
