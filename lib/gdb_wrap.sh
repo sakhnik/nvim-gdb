a2c79941313d319ec870ba1cf15dcd3d24954ef1
Anatolii Sakhnik <sakhnik@gmail.com>
2021-01-20 23:09:57 +0200

    Fix #125 Configure GDB to report absolute paths

--- lib/gdb_wrap.sh
+++ lib/gdb_wrap.sh
@@ -27,6 +27,7 @@ gdb_init=$(mktemp /tmp/gdb_init.XXXXXX)
 cat >"$gdb_init" <<EOF
 set confirm off
 set pagination off
+set filename-display absolute
 python gdb.prompt_hook = lambda p: p + ("" if p.endswith("\x1a\x1a\x1a") else "\x1a\x1a\x1a")
 EOF
 
