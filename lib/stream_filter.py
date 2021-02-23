9479c722981424eb70c58edb12578668e4da5782
Anatolii Sakhnik <sakhnik@gmail.com>
2021-02-23 19:00:23 +0200

    Fix #133 preserve debugger's colouring while running through a proxy

--- lib/stream_filter.py
+++ lib/stream_filter.py
@@ -1,6 +1,8 @@
 """Filter the stream from within given pair of tokens."""
 
 import re
+import bisect
+
 
 class Filter:
     """Pass-through filter."""
@@ -43,17 +45,47 @@ class StreamFilter(Filter):
         filtered = None
         if not self.matcher:
             return data, None
-        # Get rid of control sequences
-        data = self.CSEQ.sub(b'', data)
+
         self.buffer.extend(data)
+
+        # We'd like to preserve colouring in self.buffer, but allow filtering
+        # for the side channel without control sequences.
+
+        # Copy the buffer chunks without control sequences
+        buffer_no_cs = bytearray()
+        # Chunks offsets in self.buffer
+        offsets_buffer = []
+        # Corresponding offsets in buffer_no_cs
+        offsets_buffer_no_cs = []
+        # Current offset in self.buffer while iterating over CSEQ.
+        offset = 0
+        for m in re.finditer(self.CSEQ, self.buffer):
+            if m.start() > offset:
+                # Copy over the chunk until a CSEQ start and remember the offsets
+                offsets_buffer_no_cs.append(len(buffer_no_cs))
+                offsets_buffer.append(offset)
+                buffer_no_cs.extend(self.buffer[offset:m.start()])
+            offset = m.end()
+        # Copy the rest of the buffer into the search buffer buffer_no_cs
+        if offset < len(self.buffer):
+            offsets_buffer_no_cs.append(len(buffer_no_cs))
+            offsets_buffer.append(offset)
+            buffer_no_cs.extend(self.buffer[offset:])
+
         # Note that we are scanning over the buffer again and again
         # if this causes noticeable performance issue, consider maintaining
         # a smaller part of the buffer to scan.
-        match = self.matcher.search(self.buffer)
+        match = self.matcher.search(buffer_no_cs)
         if match:
+            # We've just found the boundaries of the desired output.
             self.matcher = None
-            filtered = bytes(self.buffer[:match.end()])
-            output = bytes(self.buffer[match.end():])
+            filtered = bytes(buffer_no_cs[:match.end()])
+            # Find corresponding offset in the original buffer to remove
+            # whatever corresponded to the found message.
+            idx = bisect.bisect_left(offsets_buffer_no_cs, match.end())
+            assert idx > 0
+            offset_rest = offsets_buffer[idx-1] + match.end() - offsets_buffer_no_cs[idx-1]
+            output = bytes(self.buffer[offset_rest:])
             self.buffer = bytearray()
             return output, filtered
         return b'', None
