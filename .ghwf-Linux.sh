6e587d36a0f814b5ad80408c6c34813b723825eb
Tyler S. Jordan <tylerscottjordan@gmail.com>
2021-09-29 21:26:02 -0600

    Only do CMake Tests on Linux
    Add gitignore for CMake build directory

--- .ghwf-Linux.sh
+++ .ghwf-Linux.sh
@@ -11,7 +11,7 @@ ln -sf "$PWD/nvim.appimage" "$HOME/bin/nvim"
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 sudo apt-get update
-sudo apt-get install gdb
+sudo apt-get install gdb cmake
 
 # Install bashdb
 ver=$(curl -sL "https://sourceforge.net/projects/bashdb/rss" \
