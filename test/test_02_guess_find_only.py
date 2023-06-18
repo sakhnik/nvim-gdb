0df43b7c29f6be89166c303c13934b4dc1f3ceba
Anatolii Sakhnik <sakhnik@gmail.com>
2023-06-18 20:39:58 +0300

    Skip cmake tests in win32 #191

--- test/test_02_guess_find_only.py
+++ test/test_02_guess_find_only.py
@@ -1,12 +1,17 @@
 import config
+import pytest
+import sys
 
 test_exec = []
 if "cmake" in config.BACKEND_NAMES:
     test_exec = ['build/cmake_test_exec']
 
+if sys.platform == 'win32':
+    pytest.skip("skipping cmake tests", allow_module_level=True)
+
 
 def test_cmake_completion(eng, cd_to_cmake):
-    execs = eng.eval("ExecsCompletion('../','','')")
-    assert(execs == test_exec+['../a.out'])
+    execs = eng.eval("ExecsCompletion('../', '', '')")
+    assert execs == test_exec + ['../a.out']
 
     assert cd_to_cmake
