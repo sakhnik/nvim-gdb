e4a9a611399cef128320903dc889fc1fd9a6e4e8
Anatolii Sakhnik <sakhnik@gmail.com>
2025-01-19 21:22:29 +0200

    Debounce jump #213

--- lua/nvimgdb/parser_actions.lua
+++ lua/nvimgdb/parser_actions.lua
@@ -6,6 +6,8 @@ local log = require 'nvimgdb.log'
 ---@class ParserActions @parser callbacks handler
 ---@field private cursor Cursor @current line sign handler
 ---@field private win Win @jump window manager
+---@field private last_fname string? last jumped file name to debounce unwanted jumps
+---@field private last_line integer? last jumped line
 local ParserActions = {}
 ParserActions.__index = ParserActions
 
@@ -18,6 +20,8 @@ function ParserActions.new(cursor, win)
   local self = setmetatable({}, ParserActions)
   self.cursor = cursor
   self.win = win
+  self.last_fname = nil
+  self.last_line = nil
   return self
 end
 
@@ -26,6 +30,8 @@ function ParserActions:continue_program()
   log.debug({"ParserActions:continue_program"})
   self.cursor:hide()
   vim.api.nvim_command("doautocmd User NvimGdbContinue")
+  self.last_fname = nil
+  self.last_line = nil
 end
 
 ---Handle the program breaked. Show the source code.
@@ -33,8 +39,14 @@ end
 ---@param line number line number
 function ParserActions:jump_to_source(fname, line)
   log.debug({"ParserActions:jump_to_source", fname = fname, line = line})
-  self.win:jump(fname, line)
-  vim.api.nvim_command("doautocmd User NvimGdbBreak")
+  if fname ~= self.last_fname or line ~= self.last_line then
+    self.last_fname = fname
+    self.last_line = line
+    self.win:jump(fname, line)
+    vim.api.nvim_command("doautocmd User NvimGdbBreak")
+  else
+    log.debug({"ignore repeated jump"})
+  end
 end
 
 ---It's high time to query actual breakpoints.
